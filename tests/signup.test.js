4d2f48c365e19773e6d3a0a18910fabd54ec991d
jkcon <github@xiixii.pw>
2019-02-14 23:10:34 -0600

    First

---
+++ tests/signup.test.js
@@ -0,0 +1,65 @@
+/* global page */
+/* eslint-disable no-global-assign */
+
+const faker = require('faker')
+
+describe('Signup', () => {
+  const randomName = faker.name.firstName()
+  const randomEmail = faker.internet.email()
+  const randomPassword = faker.internet.password()
+
+  beforeAll(async () => {
+    try {
+      await page.goto('https://ace-web-stg.herokuapp.com/user/signup')
+    } catch (e) {
+      throw new Error(e)
+    }
+  })
+
+  afterAll(async () => {
+    try {
+      await page.close()
+      page = await browser.newPage()
+    } catch (e) {
+      throw new Error(e)
+    }
+  })
+
+  it('User should signup using new credentials', async () => {
+    try {
+      console.log({ randomEmail, randomPassword, randomName })
+      await page.type('#email', randomEmail)
+      await page.type('#firstName', randomName)
+      await page.type('#password', randomPassword)
+      await page.click('select.form-control')
+      await page.type('select.form-control', 'Texas Pre-License Required - Law of Agency')
+      await page.screenshot({ path: './tests/screenshots/signup.png' })
+      await page.click('[type="submit"]')
+      await page.waitForNavigation()
+      await page.screenshot({ path: './tests/screenshots/signup-after.png' })
+      await expect(page).toMatch('Texas Pre-License Required - Law of Agency')
+    } catch (e) {
+      throw new Error(e)
+    }
+  })
+
+  it('User should be able to complete $99 purchase', async () => {
+    try {
+      await page.click('button.ace-btn')
+      await page.waitForNavigation()
+      await expect(page).toFillForm('form[name="checkout"]', {
+        number: '4242424242424242',
+        expiry: '12/22',
+        cvc: '999'
+      })
+      await page.click('#tandc')
+      await page.screenshot({ path: './tests/screenshots/purchase.png' })
+      await page.click('[type="submit"]')
+      await page.waitForSelector('section[class="content-block"]')
+      await page.screenshot({ path: './tests/screenshots/done.png' })
+      await expect(page).toClick('button[value="Continue"]')
+    } catch (e) {
+      throw new Error(e)
+    }
+  })
+})
